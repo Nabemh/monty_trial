2500693256af685b2709077d670341df0b9d0c80
Nabemh <nhanior@gmail.com>
2024-01-19 16:44:41 +0100

    push added

--- pall.c
+++ pall.c
@@ -7,7 +7,7 @@
  *
  */
 
-void h_pall(stack_t **head, unsigned int counter)
+void f_pall(stack_t **head, unsigned int counter)
 {
 	stack_t *temp;
 	(void) counter;
@@ -23,4 +23,46 @@ void h_pall(stack_t **head, unsigned int counter)
 		printf("%d\n", temp->n);
 		temp = temp->next;
 	}
+}
+
+/**
+ * f_push - add node to the stack
+ * @head: pointer to head stack
+ * @counter: line number where push is called
+ *
+ */
+
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		fot (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1;
+		}
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n" counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); 
+	}
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
